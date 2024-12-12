3a4aa7f1bff6fb94795bee04c8afddf41f6c3f00
Volkoff0 <amaryt02@gmail.com>
2024-12-12 18:54:46 +0100

    just some opacity in card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,7 +6,7 @@ const Card: React.FC<{ width: string; height: string; children: React.ReactNode 
     return (
         <div
             style={{ width: `${width}px`, height: `${height}px` }}
-            className="flex flex-col bg-neutral-900 border-2 border-neutral-600/50 shadow-lg shadow-neutral-900 rounded-xl"
+            className="flex flex-col bg-neutral-900/80 border-2 border-neutral-600/50 shadow-lg shadow-neutral-900 rounded-xl"
         >
             {children}
         </div>
