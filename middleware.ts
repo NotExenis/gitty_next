7c509a8f3e9d69a0404ea0749c1ac574118e62a0
robin <r.oudeluttikhuis@resalepartners.nl>
2025-01-10 11:40:37 +0100

    login

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest ){
+
+    if(request.nextUrl.pathname.startsWith('/admin')){
+        return NextResponse.rewrite(new URL('/home', request.url))
+    }
+
+    
+
+    return NextResponse.redirect(new URL('/home', request.url))
+}
+
+export const config = {
+    matcher: [
+        '/admin/',
+        '/dashboard'
+    ]
+}
---
+++ src/app/actions/login.ts
@@ -0,0 +1,41 @@
+'use server'
+import { createSession } from '../private/session'
+import { formState, loginSchema, User } from '../../../interfaces/interfaces'
+import { connect } from '../private/connection';
+import { redirect } from 'next/navigation';
+
+export async function signIn(_state: formState, formData: FormData){
+    const validatedFields = loginSchema.safeParse ({
+        email: formData.get('email'),
+        password: formData.get('password'),
+    })
+console.log(2)
+    if(!validatedFields.success){
+        console.log("empty fields")
+        return {
+            errors: validatedFields.error.flatten().fieldErrors
+        }
+    }
+
+    let conn = await connect();
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const bcrypt = require('bcrypt');
+
+    const [rows] = await conn.execute<User[]>(
+        'SELECT user_email, user_password, user_id, user_role FROM tbl_users WHERE user_email = ?',
+        [email]
+    );
+
+    const { user_email, user_password, user_id, user_role } = rows[0] || {};
+
+    const isMatch = await bcrypt.compare(password, user_password);
+
+    if(!isMatch){
+        console.log('mismatch')
+        return //TODO:: return error message email/password are incorrect
+    }
+console.log(1)
+    await createSession(user_id, user_role)
+    redirect('/loggedin/profile')
+}
